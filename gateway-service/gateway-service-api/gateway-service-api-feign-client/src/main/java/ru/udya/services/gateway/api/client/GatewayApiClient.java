1e1bdecf6f955e8f5868afa49b6618b3b578d19e
Dmitry Mavrichev <d.mavrichev@haulmont.com>
2022-03-30 14:47:54 +0400

    Update API schema for API Gateway

--- gateway-service/gateway-service-api/gateway-service-api-feign-client/src/main/java/ru/udya/services/gateway/api/client/GatewayApiClient.java
+++
@@ -1,7 +0,0 @@
-package ru.udya.services.gateway.api.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "gateway-service")
-public interface GatewayApiClient {
-}
