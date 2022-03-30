9ba83d2aae4370fa9d30bfe3c5f8208ca43732f8
Ilya Kuchmin <kuchmin@haulmont.com>
2022-03-30 13:19:36 +0400

    Add working /auth endpoint on gateway

--- gateway-service/src/main/java/ru/udya/services/gateway/security/config/OAuth2ResourceServerSecurityConfiguration.java
+++ gateway-service/src/main/java/ru/udya/services/gateway/security/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -4,20 +4,23 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
-@EnableWebSecurity
-public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//@EnableWebSecurity
+public class OAuth2ResourceServerSecurityConfiguration /*extends WebSecurityConfigurerAdapter*/ {
 
 //    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
 
-    @Override
+//    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(ar -> ar
                         .antMatchers(HttpMethod.GET, "/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .csrf(CsrfConfigurer::disable);
     }
 
 //    @Bean
---
+++ gateway-service/src/main/java/ru/udya/services/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package ru.udya.services.gateway.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .authorizeExchange(ae -> ae
+                        .pathMatchers("/auth/realms/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerSpec::jwt)
+                .csrf(CsrfSpec::disable);
+
+        return http.build();
+    }
+}
