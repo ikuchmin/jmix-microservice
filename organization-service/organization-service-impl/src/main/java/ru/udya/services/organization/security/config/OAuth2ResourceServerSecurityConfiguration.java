806cad13fda08da2431b5a44e38926e41ead5cad
Ilya Kuchmin <kuchmin@haulmont.com>
2022-03-30 19:18:12 +0400

    Add checking jwt tokens for organization service

--- organization-service/organization-service-impl/src/main/java/ru/udya/services/organization/security/config/OAuth2ResourceServerSecurityConfiguration.java
+++ organization-service/organization-service-impl/src/main/java/ru/udya/services/organization/security/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -1,28 +1,52 @@
 package ru.udya.services.organization.security.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.SupplierJwtDecoder;
 
-//@EnableWebSecurity
-//public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-////    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated())
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-//    }
-//
-////    @Bean
-////    JwtDecoder jwtDecoder() {
-////        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-////    }
-//}
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final OAuth2ResourceServerProperties.Jwt jwtProperties;
+
+    OAuth2ResourceServerSecurityConfiguration(OAuth2ResourceServerProperties properties) {
+        this.jwtProperties = properties.getJwt();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+    @Bean
+    SupplierJwtDecoder supplierJwtDecoder() {
+        return new SupplierJwtDecoder(this::jwtDecoder);
+    }
+
+    // Using custom jwtDecoder to avoid valida issuer. It is different
+    // for the jwt getting through gateway and openid configuration
+    // getting from auth service
+    private JwtDecoder jwtDecoder() {
+        var jwtDecoder = (NimbusJwtDecoder) JwtDecoders
+                .fromIssuerLocation(this.jwtProperties.getIssuerUri());
+
+        jwtDecoder.setJwtValidator(JwtValidators.createDefault());
+
+        return jwtDecoder;
+    }
+}
