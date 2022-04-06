836d4e13d0f90f8561e322848c7cc9209a2ac12f
Ilya Kuchmin <kuchmin@haulmont.com>
2022-04-06 13:30:56 +0400

    Disable CORS on Gateway

--- gateway-service/gateway-service-api-impl/src/main/java/ru/udya/services/gateway/security/config/SecurityConfiguration.java
+++ gateway-service/gateway-service-api-impl/src/main/java/ru/udya/services/gateway/security/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.udya.services.gateway.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.config.web.server.ServerHttpSecurity.OAuth2ResourceServerSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -18,7 +19,8 @@ public class SecurityConfiguration {
                         .pathMatchers("/actuator/**").permitAll()
                         .anyExchange().authenticated())
                 .oauth2ResourceServer(OAuth2ResourceServerSpec::jwt)
-                .csrf(CsrfSpec::disable);
+                .csrf(CsrfSpec::disable)
+                .cors(CorsSpec::disable);
 
         return http.build();
     }
