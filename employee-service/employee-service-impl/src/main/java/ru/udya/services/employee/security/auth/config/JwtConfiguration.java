4e103bfec54f5bc858c5c3b1b6279ec1e960cf8d
Ilya Kuchmin <kuchmin@haulmont.com>
2022-04-06 18:23:51 +0400

    Add correct role mapping based on JWT token (realm/resource roles) for employee-service

--- employee-service/employee-service-impl/src/main/java/ru/udya/services/employee/security/auth/config/JwtConfiguration.java
+++ employee-service/employee-service-impl/src/main/java/ru/udya/services/employee/security/auth/config/JwtConfiguration.java
@@ -1,5 +1,13 @@
 package ru.udya.services.employee.security.auth.config;
 
+import io.jmix.oidc.OidcProperties;
+import io.jmix.oidc.claimsmapper.ClaimsRolesMapper;
+import io.jmix.oidc.claimsmapper.DefaultClaimsRolesMapper;
+import io.jmix.security.role.ResourceRoleRepository;
+import io.jmix.security.role.RowLevelRoleRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +16,13 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.SupplierJwtDecoder;
+import ru.udya.services.employee.security.auth.claimsmapper.KeycloakClaimsRolesMapper;
 
 @Configuration(proxyBeanMethods = false)
 public class JwtConfiguration {
+
+    @Value("${spring.security.oauth2.resourceserver.id:}")
+    protected String resourceId;
 
     private final OAuth2ResourceServerProperties.Jwt jwtProperties;
 
@@ -33,5 +45,16 @@ public class JwtConfiguration {
         jwtDecoder.setJwtValidator(JwtValidators.createDefault());
 
         return jwtDecoder;
+    }
+
+    @Bean
+    @ConditionalOnBean(ResourceRoleRepository.class)
+    public ClaimsRolesMapper claimsRoleMapper(ResourceRoleRepository resourceRoleRepository,
+                                              RowLevelRoleRepository rowLevelRoleRepository) {
+
+        var mapper = new KeycloakClaimsRolesMapper(resourceRoleRepository, rowLevelRoleRepository);
+        mapper.setResourceId(resourceId);
+
+        return mapper;
     }
 }
