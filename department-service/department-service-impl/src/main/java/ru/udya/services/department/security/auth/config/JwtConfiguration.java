505073a9dc21c1eed495832982de6203ff96559d
Ilya Kuchmin <kuchmin@haulmont.com>
2022-04-07 10:48:03 +0400

    Add correct role mapping based on JWT token (realm/resource roles) for department-service

--- department-service/department-service-impl/src/main/java/ru/udya/services/department/security/auth/config/JwtConfiguration.java
+++ department-service/department-service-impl/src/main/java/ru/udya/services/department/security/auth/config/JwtConfiguration.java
@@ -1,7 +1,12 @@
 package ru.udya.services.department.security.auth.config;
 
+import io.jmix.oidc.claimsmapper.ClaimsRolesMapper;
 import io.jmix.oidc.jwt.JmixJwtAuthenticationConverter;
 import io.jmix.oidc.usermapper.OidcUserMapper;
+import io.jmix.security.role.ResourceRoleRepository;
+import io.jmix.security.role.RowLevelRoleRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +15,14 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.SupplierJwtDecoder;
+import ru.udya.services.department.security.auth.claimsmapper.KeycloakClaimsRolesMapper;
 import ru.udya.services.department.security.auth.jwt.ExtJmixJwtAuthenticationConverter;
 
 @Configuration(proxyBeanMethods = false)
 public class JwtConfiguration {
+
+    @Value("${spring.security.oauth2.resourceserver.id:}")
+    protected String resourceId;
 
     private final OAuth2ResourceServerProperties.Jwt jwtProperties;
 
@@ -41,5 +50,16 @@ public class JwtConfiguration {
         jwtDecoder.setJwtValidator(JwtValidators.createDefault());
 
         return jwtDecoder;
+    }
+
+    @Bean
+    @ConditionalOnBean(ResourceRoleRepository.class)
+    public ClaimsRolesMapper claimsRoleMapper(ResourceRoleRepository resourceRoleRepository,
+                                              RowLevelRoleRepository rowLevelRoleRepository) {
+
+        var mapper = new KeycloakClaimsRolesMapper(resourceRoleRepository, rowLevelRoleRepository);
+        mapper.setResourceId(resourceId);
+
+        return mapper;
     }
 }
