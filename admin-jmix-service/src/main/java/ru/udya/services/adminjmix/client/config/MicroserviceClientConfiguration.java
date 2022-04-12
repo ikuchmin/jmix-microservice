691ab56c91cc4f653cdaec7f17c439499ed63e13
Ilya Kuchmin <kuchmin@haulmont.com>
2022-04-12 19:49:57 +0400

    Add relaying access token to resource server

--- admin-jmix-service/src/main/java/ru/udya/services/adminjmix/client/config/MicroserviceClientConfiguration.java
+++ admin-jmix-service/src/main/java/ru/udya/services/adminjmix/client/config/MicroserviceClientConfiguration.java
@@ -6,6 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServerBearerExchangeFilterFunction;
 import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServletBearerExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,20 +23,19 @@ import static org.springframework.beans.factory.config.ConfigurableBeanFactory.S
 
 @Configuration(proxyBeanMethods = false)
 @Import(GatewayApiClientConfiguration.class)
-@EnableFeignClients(basePackages = {"ru.udya.services.adminjmix"})
 public class MicroserviceClientConfiguration {
 
     @Bean
     @Scope(SCOPE_PROTOTYPE)
     @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        var serverBearer = new ServerBearerExchangeFilterFunction();
-        var servletBearer = new ServletBearerExchangeFilterFunction();
-        var jmixBearer = new JmixServletBearerExchangeFilterFunction();
+    public WebClient.Builder webClientBuilder(ClientRegistrationRepository clientRegistrations,
+                                              OAuth2AuthorizedClientRepository authorizedClients) {
+
+        var oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                clientRegistrations, authorizedClients);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
 
         return WebClient.builder()
-                .filter(jmixBearer)
-                .filter(servletBearer)
-                .filter(serverBearer); // relay auth token
+                .apply(oauth2.oauth2Configuration()); // relay auth token
     }
 }
