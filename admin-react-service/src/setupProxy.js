c8693fc2005d99ef354d8def423f98eafe183558
oleg <o.gavrilov@haulmont.com>
2022-04-15 14:53:30 +0400

    Fix setupProx

--- admin-react-service/src/setupProxy.js
+++ admin-react-service/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(
-        '/auth',
+        process.env.REACT_APP_API_URL,
         createProxyMiddleware({
             target: process.env.REACT_APP_PROXY_IP,
             secure: false,
