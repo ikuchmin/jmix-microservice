b90d02fc1e5235ca062d9f24fe23770a045831d9
Ilya Kuchmin <kuchmin@haulmont.com>
2022-10-17 17:28:06 +0400

    Add Spring Boot Admin service to Kubernetes cluster

--- admin-service/src/main/java/ru/udya/services/admin/AdminApplication.java
+++ admin-service/src/main/java/ru/udya/services/admin/AdminApplication.java
@@ -3,8 +3,12 @@ package ru.udya.services.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 @SpringBootApplication
 public class AdminApplication {
 
